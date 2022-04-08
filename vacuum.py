6e66c123af7e986c4d29fa14b33c04bbddb1172e
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-04-07 18:02:05 -0700

    Initial setup

---
+++ vacuum.py
@@ -0,0 +1,17 @@
+#!/us#!/usr/bin/python3
+
+# AI 531 - Vacuum agents
+# Wadood Alam
+# Joe Nguyen
+# Matthew Pacey
+
+import unittest
+
+
+class TestAgents(unittest.TestCase):
+
+    def test_agent1(self):
+        print("Testing agent")
+
+if __name__ == '__main__':
+    unittest.main()
