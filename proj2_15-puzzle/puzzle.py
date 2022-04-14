f39ae1ac2a50713af7876fee70d848cd55fed554
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-04-14 16:47:09 -0700

    Moved vacuum agent into separate folder; created new folder for project 2 - 15 puzzle

---
+++ proj2_15-puzzle/puzzle.py
@@ -0,0 +1,22 @@
+#!/us#!/usr/bin/python3
+
+# AI 531 - Project 2 - 15 Puzzle
+# Wadood Alam
+# Joe Nguyen
+# Matthew Pacey
+
+import random
+import unittest
+
+"""
+TODO: Define classes/tests
+"""
+
+class TestPuzzle(unittest.TestCase):
+
+    def test_puzzle(self):
+        print("TODO Test")
+
+
+if __name__ == '__main__':
+    unittest.main()
