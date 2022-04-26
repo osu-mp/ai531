693c1f7a72242c1e429b84f2d2b1ce20d3d3b19a
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-04-26 10:23:47 -0700

    Update linear_conflict.py
    Removed unused funcs and printouts

--- proj2_15-puzzle/linear_conflict.py
+++ proj2_15-puzzle/linear_conflict.py
@@ -1,48 +1,7 @@
 import time
-
 import main
+
 from puzzle import Puzzle, emptySquare, puzzleSize, heuristicCityBlock
-
-
-# def linear_conflicts(candidate, solved, size):
-#     def count_conflicts(candidate_row, solved_row, size, ans=0):
-#         counts = [0 for x in range(size)]
-#         for i, tile_1 in enumerate(candidate_row):
-#             if tile_1 in solved_row and tile_1 != 0:
-#                 solved_i = solved_row.index(tile_1)
-#                 for j, tile_2 in enumerate(candidate_row):
-#                     if tile_2 in solved_row and tile_2 != 0 and i != j:
-#                         solved_j = solved_row.index(tile_2)
-#                         if solved_i > solved_j and i < j:
-#                             counts[i] += 1
-#                         if solved_i < solved_j and i > j:
-#                             counts[i] += 1
-#         if max(counts) == 0:
-#             return ans * 2
-#         else:
-#             i = counts.index(max(counts))
-#             candidate_row[i] = -1
-#             ans += 1
-#             return count_conflicts(candidate_row, solved_row, size, ans)
-#
-#     res = heuristicCityBlock(candidate)
-#     candidate_rows = [[] for y in range(size)]
-#     candidate_columns = [[] for x in range(size)]
-#     solved_rows = [[] for y in range(size)]
-#     solved_columns = [[] for x in range(size)]
-#     for y in range(size):
-#         for x in range(size):
-#             idx = (y * size) + x
-#             candidate_rows[y].append(candidate[idx])
-#             candidate_columns[x].append(candidate[idx])
-#             solved_rows[y].append(solved[idx])
-#             solved_columns[x].append(solved[idx])
-#     for i in range(size):
-#         res += count_conflicts(candidate_rows[i], solved_rows[i], size)
-#     for i in range(size):
-#         res += count_conflicts(candidate_columns[i], solved_columns[i], size)
-#     return res
-
 
 def count_conflicts_row(puzzle: Puzzle, row_id: int):
     res = 0
@@ -67,7 +26,6 @@ def count_conflicts_row(puzzle: Puzzle, row_id: int):
 
     return res
 
-
 def count_conflicts_col(puzzle: Puzzle, col_id: int):
     res = 0
     for i in range(puzzleSize):
@@ -91,9 +49,10 @@ def count_conflicts_col(puzzle: Puzzle, col_id: int):
 
     return res
 
-
 def heuristicMy(puzzle: Puzzle):
-    # global heuristicTime
+    """
+    Count the total conflicts in the puzzle (rows + cols) and add this to the city block estimate
+    """
     start = time.time()
     cnt_conflicts = 0
     city_block = heuristicCityBlock(puzzle)
@@ -102,6 +61,7 @@ def heuristicMy(puzzle: Puzzle):
 
     for i in range(puzzleSize):
         cnt_conflicts += count_conflicts_col(puzzle, i)
+    
     # print(f'{res=}')
     main.heuristicTime += time.time() - start
 
